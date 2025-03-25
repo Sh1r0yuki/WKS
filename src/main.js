20df5dd8490b77698f6f39a443b2ecb04cd4e83a
RoRo141414 <146880906+RoRo141414@users.noreply.github.com>
2025-03-25 15:38:19 +0100

    Commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+// Importation du fichier CSS principal
 import './assets/main.css'
 
+// Importation de Vue et du composant racine
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Création et montage de l'application Vue
 createApp(App).mount('#app')
